12945440db8fc0ebaa2ce0367ab3c97575df21c4
nseniak <noamseniak@gmail.com>
2023-01-05 20:55:11 +0100

    added light management

--- srcs/image_calculation/plane.c
+++ srcs/image_calculation/plane.c
@@ -6,16 +6,15 @@
 /*   By: nseniak <nseniak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:38:23 by nseniak           #+#    #+#             */
-/*   Updated: 2022/12/23 14:59:59 by nseniak          ###   ########.fr       */
+/*   Updated: 2023/01/05 18:26:38 by nseniak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_point	*closest_plane(t_minirt *minirt, t_vect *v, t_point *point)
+void	closest_plane(t_minirt *minirt, t_vect v, t_point *closest)
 {
 	(void)v;
 	(void)minirt;
-	(void)point;
-	return (NULL);
+	(void)closest;
 }
