8605bac1d7f35686ad5ccc443fd0f1fd4e1ca067
nseniak <noamseniak@gmail.com>
2023-01-06 18:40:12 +0100

    added planes

--- srcs/image_calculation/cylinder.c
+++ srcs/image_calculation/cylinder.c
@@ -6,15 +6,36 @@
 /*   By: nseniak <nseniak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:38:15 by nseniak           #+#    #+#             */
-/*   Updated: 2023/01/05 18:26:16 by nseniak          ###   ########.fr       */
+/*   Updated: 2023/01/06 18:39:53 by nseniak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void closest_cylinder(t_minirt *minirt, t_vect v, t_point *closest)
+void	cylinder_inter(t_vect v, t_minirt *minirt, t_cylinder *cylinder, t_point *closest)
 {
-	(void)minirt;
+	// t_vect	tmp;
+	// float	t;
+
+	// closest->pos = tmp;
+	// closest->raw_colour = cylinder->rgb;
+	// closest->normal = cylinder->dir;
+	// closest->init = 1;
 	(void)v;
+	(void)minirt;
+	(void)cylinder;
 	(void)closest;
+	return ;
+}
+
+void	closest_cylinder(t_minirt *minirt, t_vect v, t_point *closest)
+{
+	t_list	*cylinders;
+
+	cylinders = minirt->scene->cylinder;
+	while (cylinders)
+	{
+		cylinder_inter(v, minirt, (t_cylinder *)(cylinders->value), closest);
+		cylinders = cylinders->next;
+	}
 }
--- srcs/image_calculation/plane.c
+++ srcs/image_calculation/plane.c
@@ -6,15 +6,46 @@
 /*   By: nseniak <nseniak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:38:23 by nseniak           #+#    #+#             */
-/*   Updated: 2023/01/05 18:26:38 by nseniak          ###   ########.fr       */
+/*   Updated: 2023/01/06 18:20:01 by nseniak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+void	plane_inter(t_vect v, t_minirt *minirt, t_plane *plane, t_point *closest)
+{
+	t_vect	tmp;
+	float	a;
+	float	b;
+	float	t;
+
+	a = dot(plane->dir, sub(minirt->scene->cam.pos, plane->pos));
+	b = dot(plane->dir, v);
+	if (b == 0)
+		return ;
+	t = - a / b;
+	if (t < 0)
+		return ;
+	tmp.x = t * v.x + minirt->scene->cam.pos.x;	
+	tmp.y = t * v.y + minirt->scene->cam.pos.y;
+	tmp.z = t * v.z + minirt->scene->cam.pos.z;
+	if (closest->init && distance(tmp, minirt->scene->cam.pos) > distance(closest->pos, minirt->scene->cam.pos))
+		return ;
+	closest->pos = tmp;
+	closest->raw_colour = plane->rgb;
+	closest->normal = plane->dir;
+	closest->init = 1;
+	return ;
+}
+
 void	closest_plane(t_minirt *minirt, t_vect v, t_point *closest)
 {
-	(void)v;
-	(void)minirt;
-	(void)closest;
+	t_list	*planes;
+
+	planes = minirt->scene->plane;
+	while (planes)
+	{
+		plane_inter(v, minirt, (t_plane *)(planes->value), closest);
+		planes = planes->next;
+	}
 }
