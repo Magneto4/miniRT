e12d97d38dd23921cd574a22bd564383e38703e1
noegrx <giroux.noe@gmail.com>
2023-01-08 17:49:17 +0100

    add string conversion

---
+++ srcs/pkg/string_conv.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_conv.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngiroux <ngiroux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 17:02:47 by ngiroux           #+#    #+#             */
+/*   Updated: 2023/01/08 17:46:12 by ngiroux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "minirt.h"
+#include <stdio.h>
+
+double	__pow(double nb, double power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * __pow(nb, power - 1));
+}
+
+int	__atoi(char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' || \
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		str++;
+	}
+	return (res * sign);
+}
+
+long int	__atol(char *str)
+{
+	long int	res;
+	long int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' || \
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		str++;
+	}
+	return (res * sign);
+}
+
+double	__atod(char *str)
+{
+	double	res;
+	int		sign;
+	int		dec;
+
+	sign = 1;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	res = 0;
+	res += __atol(str);
+	while (*str && *str >= '0' && *str <= '9')
+		str++;
+	if (*str == '.')
+		str++;
+	else
+		return (res * sign);
+	dec = 0;
+	while (*(str + dec) && *(str + dec) >= '0' && *(str + dec) <= '9')
+		dec++;
+	if (dec)
+		res += __atol(str) / __pow(10, dec);
+	return (res * sign);
+}
