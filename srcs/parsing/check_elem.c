71b0e86ab3da779724e155a352750d20a7618282
Noe GIROUX <ngiroux@student.42.fr>
2023-01-12 17:34:36 +0100

    add print scene, protection for types done

--- srcs/parsing/check_elem.c
+++ srcs/parsing/check_elem.c
@@ -1,35 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check.c                                            :+:      :+:    :+:   */
+/*   check_elem.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngiroux <ngiroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:57:05 by ngiroux           #+#    #+#             */
-/*   Updated: 2023/01/10 14:03:00 by ngiroux          ###   ########.fr       */
+/*   Updated: 2023/01/12 17:33:23 by ngiroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-// bool	check_ambiant(char **data)
-// {
-// 	if (__wordcount(data) != 3)
-// 		return (false);
-// 	if (__isdouble(data[1]) == false)
-// 		return (false);
-// 	if (check_rgb(data[2]) == false)
-// 		return (false);
-// 	return (true);
-// }
-
-// bool	check_camera(char **data)
-// {
-// 	// if (__wordcount(data) != 4)
-// 	// 	return (false);
-// 	// if (!__isdouble(data[3]))
-// 	// 	return (false);
-// 	// if (!__check_vector(data + 1))
-// 	// 	return (false);
-// 	// return (true);
-// }
