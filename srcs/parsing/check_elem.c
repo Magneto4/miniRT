869f66b9d9c8550ee223f9502b2fea328ecff6d3
Noe GIROUX <ngiroux@student.42.fr>
2023-01-12 18:03:16 +0100

    check for elem (muted code because fail)

--- srcs/parsing/check_elem.c
+++ srcs/parsing/check_elem.c
@@ -6,9 +6,19 @@
 /*   By: ngiroux <ngiroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:57:05 by ngiroux           #+#    #+#             */
-/*   Updated: 2023/01/12 17:33:23 by ngiroux          ###   ########.fr       */
+/*   Updated: 2023/01/12 17:54:52 by ngiroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+bool	check_ambiant(char **data)
+{
+	if (wordcount_set(data[0], WHITE_SPACE) != 2)
+		return (false);
+	if (check_double(data[0]) == false)
+		return (false);
+	if (__atod(data[0]) < 0 || __atod(data[0]) > 1)
+		return (false);
+	return (true);
+}
