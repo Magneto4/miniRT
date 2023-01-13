476139456a08217b46b17e8fcd114561c01fc57f
Noe GIROUX <ngiroux@student.42.fr>
2023-01-13 18:50:16 +0100

    own files for parsing

--- incs/minirt.h
+++ incs/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nseniak <nseniak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ngiroux <ngiroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 13:39:52 by ngiroux           #+#    #+#             */
-/*   Updated: 2023/01/12 23:53:24 by nseniak          ###   ########.fr       */
+/*   Updated: 2023/01/13 18:48:54 by ngiroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,36 +27,15 @@
 # include "elem.h"
 # include "../mlx/mlx.h"
 
+# include "parse.h"
+
 // 🕵️‍♂️ Define
 # define HEIGHT 900
 # define WIDTH 1500
 
-# define WHITE_SPACE " \t\n"
-# define MAX_INT 2147483647
-# define MIN_INT -2147483648
-
 # ifndef BONUS
 #  define BONUS 0
 # endif
-
-// 🏳️ Parse
-t_scene	*__parse(char *file);
-
-bool	set_couple(char **data, t_couple *couple);
-bool	set_vector(char **data, t_vect *vector);
-bool	set_rgb(char **data, t_rgb *rgb);
-
-void	set_ambiant(char **data, t_scene *scene);
-void	set_camera(char **data, t_scene *scene);
-void	set_light(char **data, t_scene *scene);
-void	set_sphere(char **data, t_scene *scene);
-void	set_plane(char **data, t_scene *scene);
-void	set_cylinder(char **data, t_scene *scene);
-
-bool	check_double(char *str);
-bool	check_int(char *str);
-
-bool	check_ambiant(char **data);
 
 // 🏔️ MiniRT
 
--- incs/parse.h
+++ incs/parse.h
@@ -6,11 +6,34 @@
 /*   By: ngiroux <ngiroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:21:39 by ngiroux           #+#    #+#             */
-/*   Updated: 2023/01/09 16:21:54 by ngiroux          ###   ########.fr       */
+/*   Updated: 2023/01/13 18:48:15 by ngiroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
+
+# define WHITE_SPACE " \t\n"
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
+
+// 🏳️ Parse
+t_scene	*__parse(char *file);
+
+bool	set_couple(char **data, t_couple *couple);
+bool	set_vector(char **data, t_vect *vector);
+bool	set_rgb(char **data, t_rgb *rgb);
+
+void	set_ambiant(char **data, t_scene *scene);
+void	set_camera(char **data, t_scene *scene);
+void	set_light(char **data, t_scene *scene);
+void	set_sphere(char **data, t_scene *scene);
+void	set_plane(char **data, t_scene *scene);
+void	set_cylinder(char **data, t_scene *scene);
+
+bool	check_double(char *str);
+bool	check_int(char *str);
+
+bool	check_ambiant(char **data);
 
 #endif /* !PARSE_H */
